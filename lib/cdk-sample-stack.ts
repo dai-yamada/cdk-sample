e22b8212f87c4ac634071337301ebe7de9446aff
dai-yamada <dai.yamada@rakus-partners.co.jp>
2024-10-04 13:43:52 +0900

    add endpoint

--- lib/cdk-sample-stack.ts
+++ lib/cdk-sample-stack.ts
@@ -1,11 +1,15 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Hitcounter } from './hitcounter';
 import { TableViewer } from 'cdk-dynamo-table-viewer';
+import { CfnDNSSEC } from 'aws-cdk-lib/aws-route53';
 
 export class CdkSampleStack extends Stack {
+  public readonly hcViewerUrl: CfnOutput;
+  public readonly hcEndpoint: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -29,6 +33,14 @@ export class CdkSampleStack extends Stack {
       title: 'Hello Hits',
       table: helloWithCounter.table,
       sortBy: '-hits',
+    });
+
+    this.hcEndpoint = new CfnOutput(this, "GatewayUrl", {
+      value: gateway.url
+    });
+
+    this.hcViewerUrl = new CfnOutput(this, "TableViewerUrl", {
+      value: tv.endpoint
     });
   }
 }
