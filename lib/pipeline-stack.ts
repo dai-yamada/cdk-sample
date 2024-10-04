0dc137aad4bd196268bab02def6655191a91543f
dai-yamada <dai.yamada@rakus-partners.co.jp>
2024-10-04 13:25:51 +0900

    add CodePipeLine

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Repository } from "aws-cdk-lib/aws-codecommit";
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class WorkshopPipelineStack extends Stack {
@@ -9,6 +10,16 @@ export class WorkshopPipelineStack extends Stack {
     // Create a CodeCommit repository called 'WorkshopRepo'
     const repo = new Repository(this, "WorkshopRepo", {
       repositoryName: "WorkshopRepo",
+    });
+
+    // The basic pipeline declaration. This sets the initial structure
+    // of our pipeline
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "WorkshopPipeline",
+      synth: new CodeBuildStep("SynthStep", {
+        input: CodePipelineSource.codeCommit(repo, "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"]
+      }),
     });
   }
 }
