d21198197893f4266f7a5696affe77bd03ef18e3
dai-yamada <dai.yamada@rakus-partners.co.jp>
2024-10-08 17:22:52 +0900

    fix cdk.yml

--- bin/cdk-sample.ts
+++ bin/cdk-sample.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkSampleStack } from '../lib/cdk-sample-stack';
+import { CdkPipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
 new CdkSampleStack(app, 'CdkSampleStack');
+new CdkPipelineStack(app, "CdkPipelineStack")
